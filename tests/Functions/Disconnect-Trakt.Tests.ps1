30c56683cab868c3e89da5474120fecc333727a1
Juni Inacio <juni.inacio@users.noreply.github.com>
2017-03-21 23:02:34 +0100

    Added Disconnect-Trakt tests

---
+++ tests/Functions/Disconnect-Trakt.Tests.ps1
@@ -0,0 +1,18 @@
+Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath '../../Trakt-PowerShell/Trakt-PowerShell.psd1') -Force -ArgumentList $true
+
+InModuleScope Trakt-PowerShell {
+    Describe "Disconnect-Trakt" -Tags "CI" {
+        BeforeAll {
+            Connect-Trakt | Out-Null
+        }
+
+        It "Remove access_token" {
+            Disconnect-Trakt
+
+            $Script:ACCESS_TOKEN_PATH | Should Not Exist
+        }
+
+        AfterAll {
+        }
+    }
+}
