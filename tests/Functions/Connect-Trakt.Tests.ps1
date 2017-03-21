f7e5457d6762bd523d09ee992cbd1519a0b53e74
Juni Inacio <juni.inacio@users.noreply.github.com>
2017-03-21 22:59:56 +0100

    Added Connect-Trakt tests

---
+++ tests/Functions/Connect-Trakt.Tests.ps1
@@ -0,0 +1,40 @@
+Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath '../../Trakt-PowerShell/Trakt-PowerShell.psd1') -Force -ArgumentList $true
+
+InModuleScope Trakt-PowerShell {
+    Describe "Connect-Trakt" -Tags "CI" {
+        BeforeAll {
+            $Script:ACCESS_TOKEN = $null
+            if (Test-Path -Path $Script:ACCESS_TOKEN_PATH -PathType Leaf) {
+                Remove-Item -Path $Script:ACCESS_TOKEN_PATH -Force -Confirm:$false
+            }
+        }
+
+        It "Exchange code for access_token" {
+            $token = Connect-Trakt
+
+            $Script:ACCESS_TOKEN_PATH | Should Exist
+            $token | Should Not BeNullOrEmpty
+        }
+
+        It "Exchange refresh_token for access_token " {
+            $epoch = Get-Date -Date "01/01/1970"
+            $yesterday = (Get-Date).AddDays(-1)
+            $seconds = [int](New-TimeSpan -Start $epoch -End $yesterday).TotalSeconds
+
+            $token = Import-Clixml -Path $Script:ACCESS_TOKEN_PATH
+            $token.expires_in = 60
+            $token.created_at = $seconds
+
+            $token | Export-Clixml -Path $Script:ACCESS_TOKEN_PATH
+
+            $newToken = Connect-Trakt
+
+            $Script:ACCESS_TOKEN_PATH | Should Exist
+            $newToken | Should Not BeNullOrEmpty
+            $newToken.access_token | Should Not Be $token.access_token
+        }
+
+        AfterAll {
+        }
+    }
+}
