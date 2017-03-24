37f43e993850644d9d40fbc5fd40972bf66ce2b8
Juni Inacio <juni.inacio@users.noreply.github.com>
2017-03-24 12:03:22 +0100

    Add Remove-TraktCollection tests

--- tests/Functions/Remove-TraktCollection.Tests.ps1
+++ tests/Functions/Remove-TraktCollection.Tests.ps1
@@ -1,32 +1,32 @@
 Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath '../../Trakt-PowerShell/Trakt-PowerShell.psd1') -Force -ArgumentList $true
 
 InModuleScope Trakt-PowerShell {
-    Describe "Update-TraktCollection" -Tags "CI" {
+    Describe "Remove-TraktCollection" -Tags "CI" {
         BeforeAll {
             Connect-Trakt | Out-Null
 
             $movie = Get-TraktMovie -Id 'the-avengers-2012' -Summary
+
             $show = Get-TraktShow -Id 'game-of-thrones' -Summary
+
+            $movie | Update-TraktCollection | Out-Null
+            $show | Update-TraktCollection | Out-Null
         }
 
         It "Add item to movies collection" {
-            $movie | Update-TraktCollection | Out-Null
+            $movie | Remove-TraktCollection | Out-Null
             
             $collection = Get-TraktCollection -Type movies
 
-            $collection | Where-Object { $_.Title -eq 'The Avengers' } | Should Not BeNullOrEmpty
-
-            $movie | Remove-TraktCollection | Out-Null
+            $collection | Where-Object { $_.Title -eq 'The Avengers' } | Should BeNullOrEmpty
         }
 
         It "Add item to shows collection" {
-            $show | Update-TraktCollection | Out-Null
+            $show | Remove-TraktCollection | Out-Null
             
             $collection = Get-TraktCollection -Type shows
 
-            $collection | Where-Object { $_.Title -eq 'Game of Thrones' } | Should Not BeNullOrEmpty
-
-            $show | Remove-TraktCollection | Out-Null
+            $collection | Where-Object { $_.Title -eq 'Game of Thrones' } | Should BeNullOrEmpty
         }
 
         AfterAll {
