2af1219c15c91ba879f1b82a40d101cb325028d7
Juni Inacio <juni.inacio@users.noreply.github.com>
2017-03-24 14:10:40 +0100

    Fixed tests

--- tests/Functions/Get-TraktPlayback.Tests.ps1
+++ tests/Functions/Get-TraktPlayback.Tests.ps1
@@ -7,39 +7,56 @@ InModuleScope Trakt-PowerShell {
 
             $movie = Get-TraktMovie -Summary -Id 'tron-legacy-2010'
 
-            Start-TraktScrobble -Movie $movie -Progress 10 | Out-Null
-            Suspend-TraktScrobble -Movie $movie -Progress 25 | Out-Null
-
             $episode = Get-TraktEpisode -Summary -Id "the-flash" -SeasonNumber 1 -EpisodeNumber 1
-            
-            Start-TraktScrobble -Episode $episode -Progress 10 | Out-Null
-            Suspend-TraktScrobble -Episode $episode -Progress 25 | Out-Null
-        }
-
-        It "Get all playback progress" {
-            $playback = Get-TraktPlayback
-
-            ($playback | Measure-Object).Count | Should Not Be 0
-            $playback | Where-Object { $_.Title -eq 'TRON: Legacy' } | Should Not BeNullOrEmpty
-            $playback | Where-Object { $_.Title -eq 'The Flash: City of Heroes' } | Should Not BeNullOrEmpty
         }
 
         It "Get movies playback progress" {
+            Start-TraktScrobble -Movie $movie -Progress 10 | Out-Null
+            Suspend-TraktScrobble -Movie $movie -Progress 25 | Out-Null
+
             $playback = Get-TraktPlayback -Type movies
 
             ($playback | Measure-Object).Count | Should Not Be 0
             $playback | Where-Object { $_.Title -eq 'TRON: Legacy' } | Should Not BeNullOrEmpty
+
+            Start-TraktScrobble -Movie $movie -Progress 25 | Out-Null
+            Stop-TraktScrobble -Movie $movie -Progress 90 | Out-Null
         }
 
         It "Get episodes playback progress" {
+            Start-TraktScrobble -Episode $episode -Progress 10 | Out-Null
+            Suspend-TraktScrobble -Episode $episode -Progress 25 | Out-Null
+
             $playback = Get-TraktPlayback -Type episodes
 
             ($playback | Measure-Object).Count | Should Not Be 0
-            $playback | Where-Object { $_.Title -eq 'The Flash: City of Heroes' } | Should Not BeNullOrEmpty
+            $playback | Where-Object { $_.Title -eq 'The Flash' } | Should Not BeNullOrEmpty
+
+            Start-TraktScrobble -Episode $episode -Progress 25 | Out-Null
+            Stop-TraktScrobble -Episode $episode -Progress 90 | Out-Null
+        }
+
+        It "Get all playback progress" {
+            Start-TraktScrobble -Movie $movie -Progress 10 | Out-Null
+            Suspend-TraktScrobble -Movie $movie -Progress 25 | Out-Null
+            Start-TraktScrobble -Episode $episode -Progress 10 | Out-Null
+            Suspend-TraktScrobble -Episode $episode -Progress 25 | Out-Null
+
+            $playback = Get-TraktPlayback
+
+            ($playback | Measure-Object).Count | Should Not Be 0
+            $playback | Where-Object { $_.Title -eq 'TRON: Legacy' } | Should Not BeNullOrEmpty
+            $playback | Where-Object { $_.Title -eq 'The Flash' } | Should Not BeNullOrEmpty
+
+            Start-TraktScrobble -Movie $movie -Progress 25 | Out-Null
+            Stop-TraktScrobble -Movie $movie -Progress 90 | Out-Null
+            Start-TraktScrobble -Episode $episode -Progress 25 | Out-Null
+            Stop-TraktScrobble -Episode $episode -Progress 90 | Out-Null
         }
 
         AfterAll {
             Get-TraktPlayback | Remove-TraktPlayback | Out-Null
+            Get-TraktHistory | Remove-TraktHistory | Out-Null
         }
     }
 }
