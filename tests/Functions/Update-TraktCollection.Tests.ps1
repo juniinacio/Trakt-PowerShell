00a94a178c5cdb1b8172a337498b6055b0166565
Juni Inacio <juni.inacio@users.noreply.github.com>
2017-03-24 11:58:09 +0100

    Add Update-TraktCollection tests

---
+++ tests/Functions/Update-TraktCollection.Tests.ps1
@@ -0,0 +1,35 @@
+Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath '../../Trakt-PowerShell/Trakt-PowerShell.psd1') -Force -ArgumentList $true
+
+InModuleScope Trakt-PowerShell {
+    Describe "Update-TraktCollection" -Tags "CI" {
+        BeforeAll {
+            Connect-Trakt | Out-Null
+
+            $movie = Get-TraktMovie -Id 'the-avengers-2012' -Summary
+            $show = Get-TraktShow -Id 'game-of-thrones' -Summary
+        }
+
+        It "Add item to movies collection" {
+            $movie | Update-TraktCollection
+            
+            $collection = Get-TraktCollection -Type movies
+
+            $collection | Where-Object { $_.Title -eq 'The Avengers' } | Should Not BeNullOrEmpty
+
+            $movie | Remove-TraktCollection
+        }
+
+        It "Add item to shows collection" {
+            $show | Update-TraktCollection
+            
+            $collection = Get-TraktCollection -Type shows
+
+            $collection | Where-Object { $_.Title -eq 'Game of Thrones' } | Should Not BeNullOrEmpty
+
+            $show | Remove-TraktCollection
+        }
+
+        AfterAll {
+        }
+    }
+}
